8e94674a98031f2e9205f0b28b86fada9fe8d103
Ernie Phillips <phillips.ernie@student.ccm.edu>
2022-05-20 14:15:02 -0400

    Modified a few items. Not sure how I feel about using this as a request and hit on the database is being sent every second and if you multiply that by many users, we will experience latency issues.

--- EFNotify/EntityCache.cs
+++ EFNotify/EntityCache.cs
@@ -5,81 +5,67 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Caching;
 
-namespace EFChangeNotify
-{
-    public class EntityCache<TEntity, TDbContext>
-        : IDisposable
-        where TDbContext : DbContext, new()
-        where TEntity : class
-    {
-        private DbContext _context;
-        private Expression<Func<TEntity, bool>> _query;
+namespace EFChangeNotify {
+  public class EntityCache<TEntity, TDbContext> : IDisposable
+      where TDbContext : DbContext,
+      new() where TEntity : class {
 
-        private string _cacheKey = Guid.NewGuid().ToString();
+    private DbContext _context;
+    private Expression<Func<TEntity, bool>> _query;
 
-        public EntityCache(Expression<Func<TEntity, bool>> query)
-        {
-            _context = new TDbContext();
-            _query = query;
-        }
+    private string _cacheKey = Guid.NewGuid().ToString();
 
-        private IEnumerable<TEntity> GetCurrent()
-        {
-            var query = _context.Set<TEntity>().Where(_query);
-
-            return query;
-        }
-
-        private IEnumerable<TEntity> GetResults()
-        {
-            List<TEntity> value = MemoryCache.Default[_cacheKey] as List<TEntity>;
-
-            if (value == null)
-            {
-                value = GetCurrent().ToList();
-
-                var changeMonitor = new EntityChangeMonitor<TEntity, TDbContext>(_query);
-
-                CacheItemPolicy policy = new CacheItemPolicy();
-
-                policy.ChangeMonitors.Add(changeMonitor);
-
-                MemoryCache.Default.Add(_cacheKey, value, policy);
-
-                Console.WriteLine("From Database...");
-            }
-            else
-            {
-                Console.WriteLine("From Cache...");
-            }
-
-            return value;
-        }
-
-        public IEnumerable<TEntity> Results
-        {
-            get
-            {
-                return GetResults();
-            }
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (_context != null)
-                {
-                    _context.Dispose();
-                    _context = null;
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+    public EntityCache(Expression<Func<TEntity, bool>> query) {
+      _context = new TDbContext();
+      _query = query;
     }
+
+    private IEnumerable<TEntity> GetCurrent() {
+      var query = _context.Set<TEntity>().Where(_query);
+
+      return query;
+    }
+
+    private IEnumerable<TEntity> GetResults() {
+      List<TEntity> value = MemoryCache.Default[_cacheKey] as List<TEntity>;
+
+      if (value == null) {
+        value = GetCurrent().ToList();
+
+        var changeMonitor = new EntityChangeMonitor<TEntity, TDbContext>(_query);
+
+        CacheItemPolicy policy = new CacheItemPolicy();
+
+        policy.ChangeMonitors.Add(changeMonitor);
+
+        MemoryCache.Default.Add(_cacheKey, value, policy);
+
+        Console.WriteLine("From Database...");
+      } else {
+        Console.WriteLine("From Cache...");
+      }
+
+      return value;
+    }
+
+    public IEnumerable<TEntity> Results {
+      get {
+        return GetResults();
+      }
+    }
+
+    protected virtual void Dispose(bool disposing) {
+      if (disposing) {
+        if (_context != null) {
+          _context.Dispose();
+          _context = null;
+        }
+      }
+    }
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+  }
 }
--- SignalR-DB/Hubs/StudentHub.cs
+++ SignalR-DB/Hubs/StudentHub.cs
@@ -17,46 +17,41 @@ namespace SignalR_DB.Hubs {
     public void GetStudentRecords() {
       //IHubContext context = GlobalHost.ConnectionManager.GetHubContext<StudentHub>();
       //context.Clients.All.showTheStudentList(StudentRepository.GetStudentRecords().ToList());
-      using (var cache = new EntityCache<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
-        Console.WriteLine("Press any key to stop listening for changes...");
 
+      //initial pull of any info
+      using (var cache = new EntityCache<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
         while (true) {
           IHubContext context = GlobalHost.ConnectionManager.GetHubContext<StudentHub>();
           context.Clients.All.showTheStudentList(cache.Results.ToList());
-          Console.WriteLine(cache.Results.Count());
           Thread.Sleep(1000);
-
-          if (Console.KeyAvailable)
-            break;
         }
       }
-      using (var notifer = new EntityChangeNotifier<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
-        notifer.Error += (sender, e) => {
-          Console.WriteLine("[{0}, {1}, {2}]:\n{3}", e.Reason.Info, e.Reason.Source, e.Reason.Type, e.Sql);
-        };
 
-        notifer.Changed += (sender, e) => {
-          Console.WriteLine(e.Results.Count());
-          IHubContext context = GlobalHost.ConnectionManager.GetHubContext<StudentHub>();
-          context.Clients.All.showTheStudentList(e.Results.ToList());
-          foreach (var p in e.Results) {
-            Console.WriteLine("  {0}", p.StudentID);
-          }
-        };
-      }
+      //set up a listener for the database which will update the broadcast
+      //using (var notifer = new EntityChangeNotifier<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
+      //  notifer.Error += (sender, e) => {
+      //    Console.WriteLine("[{0}, {1}, {2}]:\n{3}", e.Reason.Info, e.Reason.Source, e.Reason.Type, e.Sql);
+      //  };
+        
+      //  notifer.Changed += (sender, e) => {
+      //    IHubContext _context = GlobalHost.ConnectionManager.GetHubContext<StudentHub>();
+      //    _context.Clients.All.showTheStudentList(e.Results.ToList());
+      //  };
+
+      //  IHubContext context = GlobalHost.ConnectionManager.GetHubContext<StudentHub>();
+      //  context.Clients.All.showTheStudentList(notifer.Results.ToList());
+      //}
 
 
-      using (var otherNotifier = new EntityChangeNotifier<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
-        otherNotifier.Changed += (sender, e) => {
-          Console.WriteLine(e.Results.Count());
-        };
+      //using (var otherNotifier = new EntityChangeNotifier<Student, StudentDbEntities>(p => p.StudentName == "Ernie")) {
+      //  otherNotifier.Changed += (sender, e) => {
+      //    Console.WriteLine(e.Results.Count());
+      //  };
 
-        Console.WriteLine("Press any key to stop listening for changes...");
-        Console.ReadKey(true);
-      }
+      //  Console.WriteLine("Press any key to stop listening for changes...");
+      //  Console.ReadKey(true);
+      //}
 
-      Console.WriteLine("Press any key to stop...");
-      Console.ReadKey(true);
     }
 
   }
