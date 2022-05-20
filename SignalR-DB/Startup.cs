d629f13890f667aefe00c85ad4e30babd7cf5121
Ernie Phillips <phillips.ernie@student.ccm.edu>
2022-05-20 13:10:00 -0400

    Add with gitignore and all files back in

---
+++ SignalR-DB/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_DB.Startup))]
+namespace SignalR_DB {
+  public class Startup {
+    public void Configuration(IAppBuilder app) {
+      app.MapSignalR();
+    }
+  }
+}
